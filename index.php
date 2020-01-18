273852e04bab5262380ff6dde2db83e7abff67ff
Hendy <hendy.almeida@hotmail.com>
2020-01-18 10:04:19 -0300

    init front

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <img src="logo.png" alt="" class="logo">
+  </header>
+  <main>
+    <section class="categorias mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center mb-5 mt-5">
+            <h1>Categorias</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="card text-white bg-warning mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Categoria</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card text-white bg-warning mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Categoria</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card text-white bg-warning mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Categoria</h5>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card text-white bg-warning mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Categoria</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="ofertas">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center mt-5 mb-5">
+            <h2>Ofertas</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Titulo</th>
+                  <th>Preço</th>
+                  <th>Duração</th>
+                  <th>Ano de lançamento</th>
+                  <th>Classificação</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Kill Bill</td>
+                  <td>R$ 3,50</td>
+                  <td>112min</td>
+                  <td>2003</td>
+                  <td>16 anos</td>
+                </tr>
+                <tr>
+                  <td>Peixe Grande</td>
+                  <td>R$ 4,50</td>
+                  <td>125min</td>
+                  <td>2003</td>
+                  <td>12 anos</td>
+                </tr>
+                <tr>
+                  <td>Edward Mãos de Tesoura</td>
+                  <td>R$ 13,50</td>
+                  <td>105min</td>
+                  <td>1990</td>
+                  <td>Livre</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
